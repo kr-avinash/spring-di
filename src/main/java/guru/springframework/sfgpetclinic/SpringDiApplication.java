20a1df0338430ca94101d32ab1759697d136252e
kr-avinash <73075560+kr-avinash@users.noreply.github.com>
2021-11-24 11:09:32 +0530

    updated

--- src/main/java/guru/springframework/sfgpetclinic/SpringDiApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SpringDiApplication.java
@@ -2,12 +2,40 @@ package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import guru.springframework.sfgpetclinic.controller.ConstructorInjectedController;
+import guru.springframework.sfgpetclinic.controller.MyController;
+import guru.springframework.sfgpetclinic.controller.PropertyInjectedController;
+import guru.springframework.sfgpetclinic.controller.SetterInjectedController;
 
 @SpringBootApplication
 public class SpringDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+		
+		MyController myController = (MyController) ctx.getBean("myController");
+	
+		String greetings = myController.sayHello();
+		System.out.println(greetings);
+		
+		System.out.println("---- Property ----------------");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+		
+		System.out.println("---- Setter ------------");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+		
+		System.out.println("------ Constructor --------");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+		
+		
+		
+		
 	}
 
 }
